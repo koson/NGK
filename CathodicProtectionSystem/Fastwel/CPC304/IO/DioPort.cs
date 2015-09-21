d6817511f3aea8e091ec92df3e5fe5427e5012e4
Petrov Dmitriy <strimer7@yandex.ru>
2015-09-21 22:16:36 +0400

    Добавил проект для создания класса-обертки над портом дискретного ввода вывода Fastwel СPC304

---
+++ CathodicProtectionSystem/Fastwel/CPC304/IO/DioPort.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fastwel.CPC304.IO
+{
+    /// <summary>
+    /// Класс порта дискретного ввода вывода
+    /// </summary>
+    public class DioPort
+    {
+    }
+}
