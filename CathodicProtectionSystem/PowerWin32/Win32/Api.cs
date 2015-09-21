cff4359b475a1b621a653826dc09c3a55d078a91
Petrov Dmitriy <strimer7@yandex.ru>
2015-09-21 22:38:10 +0400

    Добавил проект для управления включением, выключением и перезагрузкой PC

---
+++ CathodicProtectionSystem/PowerWin32/Win32/Api.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Power.Win32
+{
+    /// <summary>
+    /// Класс вызов функций Win32 для включения, 
+    /// выключения и перезагрузки PC
+    /// </summary>
+    public static class Api
+    {
+    }
+}
