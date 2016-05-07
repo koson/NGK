adf8c2aaea2594d2f91099b6d01b8a0053d922d0
Petrov Dmitriy <strimer7@yandex.ru>
2016-05-07 22:37:52 +0400

    Добавил полежные классы (не законченны)

---
+++ CathodicProtectionSystem/Common/Lazy.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public delegate Object Func();
+
+    public class Lazy<T>: IDisposable where T: class, new()
+    {
+        public Lazy() { }
+        public Lazy(Func initializor)
+        {
+            _Initializor = initializor;
+        }
+
+        Func _Initializor = null;
+        T _Value = null;
+        static Object SyncRoot = new object();
+
+        public T Value
+        {
+            get 
+            {
+                if (!IsValueCreated)
+                {
+                    lock(SyncRoot)
+                    {
+                        if (!IsValueCreated)
+                        {
+                            _Value = _Initializor == null ? new T() :
+                                (T)_Initializor();
+                        }
+                    }
+                }
+                return _Value;
+            }
+        }
+
+        public Boolean IsValueCreated
+        {
+            get { return _Value != null; }
+        }
+
+        public void Dispose()
+        {
+            if (IsValueCreated)
+            {
+                lock (SyncRoot)
+                {
+                    if (IsValueCreated)
+                    {
+                        if (_Value is IDisposable)
+                        {
+                            ((IDisposable)_Value).Dispose();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CathodicProtectionSystem/Common/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class Singleton<T> where T: class, new()
+    {
+        private Singleton() { }
+
+        static volatile T _Instance;
+        static Object SyncRoot = new Object();
+
+        public T Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    lock (SyncRoot)
+                    {
+                        if (_Instance == null)
+                            _Instance = new T();
+                    }
+                }
+                return _Instance;
+            }
+        }
+
+        public T GetInstance()
+        {
+            return Instance;
+        }
+    }
+}
