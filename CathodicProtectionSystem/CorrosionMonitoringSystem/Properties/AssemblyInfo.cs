214be6467740048048b16d828a982e50d969dac4
Petrov Dmitriy <strimer7@yandex.ru>
2016-08-11 11:53:40 +0300

    fix

--- CathodicProtectionSystem/CorrosionMonitoringSystem/Properties/AssemblyInfo.cs
+++ CathodicProtectionSystem/CorrosionMonitoringSystem/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Neftegazkompleks")]
 [assembly: AssemblyProduct("CorrosionMonitoringSystem")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
