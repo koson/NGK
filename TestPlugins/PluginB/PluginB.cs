4d3e9bc47a5778c3067478aade5d16ef91b9e634
Petrov Dmitriy <strimer7@yandex.ru>
2016-08-20 18:21:33 +0400

    Добавил проект плагина B

---
+++ TestPlugins/PluginB/PluginB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PluginsInfrastructure;
+using Mvp.Input;
+using Mvp.WinApplication.ApplicationService;
+using Mvp.WinApplication.Infrastructure;
+
+namespace PluginB
+{
+    public class PluginB: Plugin
+    {
+        #region Constructors
+
+        public PluginB() 
+        {
+            base.Name = "Module B";
+
+            _ActionA = new Command(OnActionA);
+
+            Menu root = new Menu("Меню модуля B", null);
+            base._Menu.Add(root);
+
+            root.SubMenuItems.Add(new Menu("Действие B", _ActionA)); 
+        }
+
+        #endregion
+
+        #region Fields And Properties
+        #endregion
+
+        #region Commands
+
+        private Command _ActionA;
+        private void OnActionA()
+        {
+            MessageBoxService.ShowInformation("Это тестовое сообщение", "Тест");
+        }
+
+        #endregion
+    }
+}
